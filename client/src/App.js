3d603b5b6a7de9c595c3c3ad86e9edfc3855a80c
Nate Grobe <nategrobe@pop-os.localdomain>
2020-11-23 15:23:07 -0500

    refactored home to its own component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
 import Employees from './components/Employees';
 import Patients from './components/Patients';
 import Nav from './components/Nav';
 import Billing from './components/Billing';
 import Covid from './components/Covid';
-import viewServices from './services/viewServices';
 
 const App = () => {
-  const [data, setData] = useState(null);
-
-  async function getView() {
-    const data = await viewServices.view1();
-    setData(data);
-  }
-
-  useEffect(() => {
-    getView();
-  }, []);
 
   return (
     <div>
@@ -33,26 +23,13 @@ const App = () => {
           <Billing />
         </Route>
 
-        <div className="grid-1-1">
-          <div className="titlePage">
-            Hospital Tech Industries
-          </div>
-          <div className="LogIn">
-                Log in
-          </div>
-          <div className="titleLower">
-            Already have a Hospital Tech Account?
-          </div>
-          <div className="titleLogin">
-            Log In
-          </div>
-        </div>
           
         <Route path='/covid'>
           <Covid />
         </Route>
         {/* this bit should display a nice welcome page */}
         <Route path='/'>
+          <Home />
         </Route>
       </Switch>
     </div>
---
+++ client/src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="grid-1-1">
+      <div className="titlePage">
+        Hospital Tech Industries
+      </div>
+      <div className="LogIn">
+            Log in
+      </div>
+      <div className="titleLower">
+        Already have a Hospital Tech Account?
+      </div>
+      <div className="titleLogin">
+        Log In
+      </div>
+    </div>
+  );
+};
+
+export default Home;
