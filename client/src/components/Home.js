8fb47cc4d00bcc8afd51ed2d871c3e9fa8187871
Nate Grobe <nategrobe@pop-os.localdomain>
2020-11-23 16:38:44 -0500

    added crappy sign in

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+  const [login, setLogin] = useState(false);
+  const [eid, setEid] = useState('');
+  const [epass, setEpass] = useState('');
+  const [signedIn, setSignedIn] = useState(false);
+
+  const showLogin = () => setLogin(true);
+
+  function handleSignIn(event) {
+    event.preventDefault();
+    if (eid.length > 0 && epass === 'pass') {
+      window.alert("Welcome!");
+      setLogin(false);
+      setEid('');
+      setEpass('');
+      setSignedIn(true);
+    } else {
+      window.alert("Invalid username or password.");
+      setEpass('');
+    }
+  }
+
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleSignIn}>
+        Employee ID: <input value={eid} onChange={({ target }) => setEid(target.value)}/>
+        <br/>
+        Password: <input type='password' value={epass} onChange={({ target }) => setEpass(target.value)}/>
+        <br/>
+        <button type='submit'>Sign in</button>
+      </form>
+    );
+  };
+
   return (
     <div className="grid-1-1">
-      <div className="titlePage">
-        Hospital Tech Industries
-      </div>
-      <div className="LogIn">
-            Log in
-      </div>
-      <div className="titleLower">
-        Already have a Hospital Tech Account?
-      </div>
-      <div className="titleLogin">
-        Log In
-      </div>
+      {login === false &&
+        <div>
+        <div className="titlePage">
+          Hospital Tech Industries
+        </div>
+          {signedIn === false &&
+          <div className="LogIn" onClick={showLogin}>
+              Log in
+          </div>}
+
+          {signedIn === true &&
+            <div>
+              Welcome
+            </div>
+          }
+
+        </div>
+        }
+      {login === true && loginForm()}
     </div>
   );
 };
