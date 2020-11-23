4276f4ba8c1d16666ae1c9c4d5d24da439324187
Nate Grobe <nategrobe@pop-os.localdomain>
2020-11-23 18:29:22 -0500

    setup queries for billing table and router

--- api/src/app.js
+++ api/src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 
 const viewRouter = require('./controllers/viewRouter');
+const tableRouter = require('./controllers/tableRouter');
 
 app.get('/', (req, res) => {
   res.send('Hello there!');
 });
 
 app.use('/api', viewRouter);
+app.use('/api/tables', tableRouter);
 
 module.exports = app;
