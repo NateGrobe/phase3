5651ca9ce5c7b20cafca346f2e1080dbe61b2436
Nate Grobe <nategrobe@pop-os.localdomain>
2020-11-24 19:48:11 -0500

    added in new middleware and routes

--- api/src/app.js
+++ api/src/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const viewRouter = require('./controllers/viewRouter');
 const tableRouter = require('./controllers/tableRouter');
+const covidRouter = require('./controllers/covidRouter');
 
 app.get('/', (req, res) => {
   res.send('Hello there!');
 });
 
+app.use(express.json());
+app.use(cors());
+
 app.use('/api', viewRouter);
+app.use('/api/covid', covidRouter);
 app.use('/api/tables', tableRouter);
 
 module.exports = app;
